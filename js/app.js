b16bcfd502c752b2d788ef963eea79e007a6aa77
usmanaqadri <usmanaqadri@gmail.com>
2022-07-09 10:08:39 -0400

    made basic class tamagotchi class with attributes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("yo");
+class Tamagotchi {
+  constructor(name) {
+    this.name = name;
+    this.hunger = 5;
+    this.sleepiness = 5;
+    this.boredom = 5;
+    this.age = 0;
+  }
+}
